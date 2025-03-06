a7b47deaaa5aa3ac9e1bc53992371914888c9911
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-03-06 20:48:45 +0900

    はじめに表示される画面を変更、React.FCを使えるように

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import PostView_Screen from './pages/PostView_Screen';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <PostView_Screen />
   </React.StrictMode>
 );
 
---
+++ src/pages/PostInput_Screen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostInput_Screen: React.FC = () => {
+  return (
+    <div>
+      <h1>作成</h1>
+    </div>
+  );
+};
+
+export default PostInput_Screen;

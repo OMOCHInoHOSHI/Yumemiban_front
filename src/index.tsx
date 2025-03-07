ec0887013daa6f725e6a6ab0a9c3d3183f6cea86
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-03-07 16:07:55 +0900

    index.tsxをルーティングに適応

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,34 @@ import PostView_Screen from './pages/PostView_Screen';
 // import PostCard from './models/PostCard';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom' // routerで追加
+import AppRoutes from './Routes'; // routerで追加
+
+// const rootElement = document.getElementById('root');
+// if (rootElement) {
+//   const root = ReactDOM.createRoot(rootElement);
+//   root.render(
+//     <React.StrictMode>
+//       <PostView_Screen />
+//     </React.StrictMode>
+//   );
+// } else {
+//   console.error('Root element not found');
+// }
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <PostView_Screen />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </React.StrictMode>
   );
 } else {
   console.error('Root element not found');
 }
+
 
 // パフォーマンス測定用の関数を追加する場合は次のように設定
 reportWebVitals();

bf25aca7ec3209f2f5b57c3bce7ecc24699edc0e
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-03-10 18:40:29 +0900

    変数名変更

--- src/models/conponent.tsx
+++ src/models/conponent.tsx
@@ -8,18 +8,18 @@ import React, { FC } from 'react';
 // 入力のコンポーネントS--------------------------------------------------------------------------
 // インターフェース　コンポーネントが受け取るプロパティの型を指定
 interface InputProps {
-    name: string;
+    input: string;
     required: boolean;
   }
 
 // コンポーネントの定義 Functional Component
 // nameは入力フィールドの初期値、requiredはその入力フィールドが必須であるかどうかを示す真偽値
 const InputForm: FC<InputProps> = (props) => {
-    const { name, required } = props;
+    const { input, required } = props;
     return (
         <div>
-            <label>ニックネーム</label>
-            <input type="text" value={name} required={required} />
+            <label>入力</label>
+            <input type="text" value={input} required={required} />
         </div>
     );
 }

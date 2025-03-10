79adb59f540622aca1dab1ba3bceef50aba3262c
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-03-10 18:43:49 +0900

    最大文字数を決めれるように

--- src/models/conponent.tsx
+++ src/models/conponent.tsx
@@ -8,18 +8,19 @@ import React, { FC } from 'react';
 // 入力のコンポーネントS--------------------------------------------------------------------------
 // インターフェース　コンポーネントが受け取るプロパティの型を指定
 interface InputProps {
-    input: string;
-    required: boolean;
+    input: string;      // 入力フィールドの初期値
+    required: boolean;  // 入力フィールドが必須かどうか
+    maxLength?: number; // 入力フィールドの最大文字数(オプション)
   }
 
 // コンポーネントの定義 Functional Component
 // nameは入力フィールドの初期値、requiredはその入力フィールドが必須であるかどうかを示す真偽値
 const InputForm: FC<InputProps> = (props) => {
-    const { input, required } = props;
+    const { input, required, maxLength } = props;
     return (
         <div>
             <label>入力</label>
-            <input type="text" value={input} required={required} />
+            <input type="text" value={input} required={required} maxLength={maxLength} />
         </div>
     );
 }

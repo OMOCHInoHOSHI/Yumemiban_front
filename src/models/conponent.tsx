6d187203a7ec4e38cb0b9ea9aeb9de1e31bb1127
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-03-10 15:53:06 +0900

    NickNameInput作成

--- src/models/conponent.tsx
+++ src/models/conponent.tsx
@@ -1,6 +1,26 @@
 // ユーザー名のコンポーネント
 
-import React from 'react';
+// import React from 'react';
+import React, { FC } from 'react';
 
-let name = "name";
+// let name = "name";
 
+// インターフェース　コンポーネントが受け取るプロパティの型を指定
+interface NickNameProps {
+    name: string;
+    required: boolean;
+  }
+
+// コンポーネントの定義 Functional Component
+// nameは入力フィールドの初期値、requiredはその入力フィールドが必須であるかどうかを示す真偽値
+const NickNameInput: FC<NickNameProps> = (props) => {
+    const { name, required } = props;
+    return (
+        <div>
+            <label>ニックネーム</label>
+            <input type="text" value={name} required={required} />
+        </div>
+    );
+}
+
+export default NickNameInput;

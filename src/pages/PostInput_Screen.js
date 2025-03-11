eb9038dbdc0473b285e0d80497e0451135fe526a
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-03-11 22:38:07 +0900

    入力のボックス作成

--- src/pages/PostInput_Screen.js
+++ src/pages/PostInput_Screen.js
@@ -3,9 +3,11 @@ import './PostInput_Screen.css';
 import Components from '../components/conponent';
 const PostInput_Screen = () => {
     return (React.createElement("div", null,
-        React.createElement("h1", null, "\u4F5C\u6210aa"),
+        React.createElement("h1", null, "\u4F5C\u6210"),
         React.createElement(Components.RoundedBox, { width: "300px", height: "200px", color: "#f0f0f0", borderRadius: "15px" },
-            React.createElement("p", null, "\u3053\u308C\u306F\u89D2\u304C\u4E38\u3044\u30DC\u30C3\u30AF\u30B9\u306E\u4E2D\u306E\u30C6\u30AD\u30B9\u30C8\u3067\u3059\u3002")),
-        React.createElement(Components.InputForm, { input: "", required: true, maxLength: 50 })));
+            React.createElement("p", null, "\u30CB\u30C3\u30AF\u30CD\u30FC\u30E0\u3092\u5165\u529B\u3057\u3066\u304F\u3060\u3055\u3044"),
+            React.createElement(Components.InputForm, { input: "", required: true, maxLength: 50 }),
+            React.createElement("p", null, "\u5922\u306E\u5185\u5BB9\u3092\u66F8\u3044\u3066\u307F\u3066\u304F\u3060\u3055\u3044...(=o=;)"),
+            React.createElement(Components.InputForm, { input: "", required: true, maxLength: 50 }))));
 };
 export default PostInput_Screen;
--- src/pages/PostInput_Screen.tsx
+++ src/pages/PostInput_Screen.tsx
@@ -7,11 +7,13 @@ import Components from '../components/conponent';
 const PostInput_Screen: React.FC = () => {
   return (
     <div>
-      <h1>作成aa</h1>
+      <h1>作成</h1>
       <Components.RoundedBox width="300px" height="200px" color="#f0f0f0" borderRadius="15px">
-        <p>これは角が丸いボックスの中のテキストです。</p>
-      </Components.RoundedBox>
+        <p>ニックネームを入力してください</p>
       <Components.InputForm input="" required={true} maxLength={50} />
+        <p>夢の内容を書いてみてください...(=o=;)</p>
+      <Components.InputForm input="" required={true} maxLength={50} />
+      </Components.RoundedBox>
     </div>
   );
 };

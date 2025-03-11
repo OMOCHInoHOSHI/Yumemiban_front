d98f5c6ef11f7ecd6454aba5ad24213c17c9f0ba
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-03-11 23:39:37 +0900

    テキストに対する処理の関数

---
+++ src/models/PushText.tsx
@@ -0,0 +1,14 @@
+// ニックネームに対する処理を行う関数
+export const pushNickname = (nickname: string) => {
+    console.log('Nickname:', nickname);
+};
+
+// 夢の内容に対する処理を行う関数
+export const pushDreamtext = (dreamContent: string) => {
+    console.log('Dream Content:', dreamContent);
+};
+
+// ニックネームを取得する関数
+export const getNickname = () => {
+    return 'nickname';
+}

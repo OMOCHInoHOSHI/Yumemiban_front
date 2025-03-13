bc17401a13bb219c7631ccd7321ac202ab1b9967
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-03-13 15:17:42 +0900

    小説テキストを表示

--- src/pages/PostInput_Screen.tsx
+++ src/pages/PostInput_Screen.tsx
@@ -1,5 +1,3 @@
-// useStateを使って、入力フォームの値を管理する
-// useState..関数コンポーネントで状態（state）を管理するために使用
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './PostInput_Screen.css';
@@ -12,7 +10,7 @@ const PostInput_Screen: React.FC = () => {
   // 初期値を空文字列に設定
   const [nickname, setNickname] = useState('');   // ニックネーム
   const [content, setContent] = useState('');     // 夢の内容
-
+  const [novelText, setNovelText] = useState(''); // 生成された短編小説のテキスト
 
   const handleSubmit = () => {
     // ニックネームと文章を textpush 関数に渡す
@@ -21,41 +19,44 @@ const PostInput_Screen: React.FC = () => {
 
   const sendcreateNovle = () => {
     console.log('createNovle');
+    const generatedText = getNoveltext();
+    setNovelText(generatedText);
   }
 
+  const getNoveltext = () => {
+    console.log('getNoveltext');
+    return 'noveltext';
+  }
 
   return (
     <div>
       <h2>作成</h2>
-      { <Components.RoundedBox width="300px" height="200px" color="#f0f0f0" borderRadius="15px">
-
+      <Components.RoundedBox width="300px" height="200px" color="#f0f0f0" borderRadius="15px">
         <p>ニックネームを入力してください</p>
         <Components.InputForm
-        input={nickname}  // useState の値を渡す
-        required={true}
-        maxLength={20}
-        onChange={setNickname}
+          input={nickname}  // useState の値を渡す
+          required={true}
+          maxLength={20}
+          onChange={setNickname}
         />
-
         <p>夢の内容を書いてみてください...(=o=;)</p>
         <Components.InputForm
-        input={content}  // useState の値を渡す
-        required={true}
-        maxLength={50}
-        onChange={setContent}
+          input={content}  // useState の値を渡す
+          required={true}
+          maxLength={50}
+          onChange={setContent}
         />
-
-    </Components.RoundedBox> }
-
-
-      {/* 生成された短編小説のページS---------------------------------------------------------------- */}
-      <Components.RoundedBox width="300px" height="200px" color="#f0f0f0" borderRadius="15px">
-       <p>生成された短編小説</p>
-        <button onClick={sendcreateNovle}>生成</button>
-
-
       </Components.RoundedBox>
-      {/* 生成された短編小説のページE---------------------------------------------------------------- */}
+
+      {/* 生成された短編小説のページS */}
+      <Components.RoundedBox width="300px" height="200px" color="#f0f0f0" borderRadius="15px">
+        <p>生成された短編小説</p>
+        <button onClick={sendcreateNovle}>生成</button>
+        <div>
+          <Components.NovelTextFild novelText={novelText} />
+        </div>
+      </Components.RoundedBox>
+      {/* 生成された短編小説のページE */}
 
       <button onClick={handleSubmit}>送信</button>
     </div>

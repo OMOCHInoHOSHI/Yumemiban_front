56540d8694ac9c8322a2502443237b3cdd79477f
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-03-13 18:44:26 +0900

    jsonファイルを適応

--- src/pages/PostInput_Screen.js
+++ src/pages/PostInput_Screen.js
@@ -5,6 +5,7 @@ import textpush from '../models/PushText';
 import Novel from '../models/Novel';
 import { IconButton } from '@mui/material';
 import { SendIcon } from 'lucide-react';
+import ja_text from '../resous_ja.json';
 const PostInput_Screen = () => {
     // 初期値を空文字列に設定
     const [nickname, setNickname] = useState(''); // ニックネーム
@@ -24,13 +25,15 @@ const PostInput_Screen = () => {
     return (React.createElement("div", null,
         React.createElement("h2", null, "\u4F5C\u6210"),
         React.createElement(Components.RoundedBox, { width: "300px", height: "200px", color: "#f0f0f0", borderRadius: "15px" },
-            React.createElement("p", null, "\u30CB\u30C3\u30AF\u30CD\u30FC\u30E0\u3092\u5165\u529B\u3057\u3066\u304F\u3060\u3055\u3044"),
+            React.createElement("p", null, ja_text.nickname_input_label),
             React.createElement(Components.InputForm, { input: nickname, required: true, maxLength: 20, onChange: setNickname }),
-            React.createElement("p", null, "\u5922\u306E\u5185\u5BB9\u3092\u66F8\u3044\u3066\u307F\u3066\u304F\u3060\u3055\u3044...(=o=;)"),
+            React.createElement("p", null,
+                ja_text.dream_content_label,
+                "...(=o=;)"),
             React.createElement(Components.InputForm, { input: content, required: true, maxLength: 50, onChange: setContent })),
         React.createElement(Components.RoundedBox, { width: "300px", height: "200px", color: "#f0f0f0", borderRadius: "15px" },
-            React.createElement("p", null, "\u751F\u6210\u3055\u308C\u305F\u77ED\u7DE8\u5C0F\u8AAC"),
-            React.createElement("button", { onClick: sendcreateNovle }, "\u751F\u6210"),
+            React.createElement("p", null, ja_text.generated_novel_label),
+            React.createElement("button", { onClick: sendcreateNovle }, ja_text.generate_button_label),
             React.createElement("div", { className: "novel-text-field" },
                 React.createElement(Components.NovelTextFild, { novelText: novelText }))),
         React.createElement(IconButton, { onClick: handleSubmit },
